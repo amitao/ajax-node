1c1641583fd65741209dbe5318e5a4e2d0b7df9e
Amie Thao <amie.c.thao@gmail.com>
2018-11-14 10:51:58 -0600

    add get method

--- server/server.js
+++ server/server.js
@@ -7,10 +7,34 @@ const app = express();
 // port that server listens to
 const PORT = 5000;
 
+let quote = ['Work is the curse of the drinking class',
+              'The only thing to fear is fear itself',
+              `fool me once, shame on me, but teach a man to fool me and...`];
+
+// return one quote not the same every time
+let index = 0;
+function getQuote(){
+
+  let quoteToReturn = quote[index];
+  // increment by 1
+  index += 1;
+
+  // keep our quote inbounds
+  if(index >= quote.length){
+    //reset index back to zero
+    index = 0;
+  }
+  return quoteToReturn;
+}
+
+
+
 // get syntax: get('route', function callback)
 // function callback syntax: function(request, respond){}
+// per request one respond back
 app.get('/quote', function(req, res){
-  res.send('got to quote');
+
+  res.send(getQuote());
 });
 
 
